eae1d38f4baa3ecdbfd98494d83263c734679389
Asem Abu Alrub <asem.m.abualrub@gmail.com>
2023-07-27 11:12:21 +0300

    Add Splash API

--- app/src/main/java/com/asemlab/quakes/MainActivity.kt
+++ app/src/main/java/com/asemlab/quakes/MainActivity.kt
@@ -2,12 +2,14 @@ package com.asemlab.quakes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
