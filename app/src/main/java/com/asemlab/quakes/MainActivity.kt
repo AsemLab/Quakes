5b1e7294386191445324bbfb3767052fafd3e1a9
Asem Abu Alrub <asem.m.abualrub@gmail.com>
2023-07-30 11:06:11 +0300

    Add Firebase notifications

--- app/src/main/java/com/asemlab/quakes/MainActivity.kt
+++ app/src/main/java/com/asemlab/quakes/MainActivity.kt
@@ -1,16 +1,67 @@
 package com.asemlab.quakes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        val TAG = MainActivity::class.simpleName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED) {
+            val myIntent =
+                Intent(
+                    Settings.ACTION_APP_NOTIFICATION_SETTINGS
+                ).putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+
+            AlertDialog.Builder(this).apply {
+                setTitle(getString(R.string.dialog_permission_title))
+                setMessage(getString(R.string.dialog_permission_text))
+                setPositiveButton(getString(R.string.dialog_permission_button)) { dialog, _ ->
+                    try {
+                        startActivity(myIntent)
+                        dialog.dismiss()
+                    } catch (e: ActivityNotFoundException) {
+                        startActivity(Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS))
+                        dialog.dismiss()
+                    }
+                }
+                setCancelable(true)
+            }.show()
+        }
+
+        getFCMToken()
+
+    }
+
+    private fun getFCMToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.e(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            if (BuildConfig.DEBUG)
+                Log.d(TAG, task.result)
+        })
     }
 }
---
+++ app/src/main/java/com/asemlab/quakes/services/NotificationsService.kt
@@ -0,0 +1,57 @@
+package com.asemlab.quakes.services
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import com.asemlab.quakes.BuildConfig
+import com.asemlab.quakes.R
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+class NotificationsService : FirebaseMessagingService() {
+    private val DEFAULT_CHANNEL_ID = "FIREBASE_NOTIFICATION"
+    private val NOTIFICATION_ID = 1001
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        message.notification?.let {
+            showNotification(it)
+        }
+    }
+
+    private fun showNotification(it: RemoteMessage.Notification) {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notification = Notification.Builder(this)
+            .setContentTitle(it.title ?: "")
+            .setContentText(it.body ?: "")
+            .setAutoCancel(true)
+            .setSmallIcon(R.drawable.ic_feeling_3)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                DEFAULT_CHANNEL_ID,
+                getString(R.string.default_notification_channel_id),
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description =
+                getString(R.string.default_notification_channel_desc)
+            notificationManager.createNotificationChannel(channel)
+            notification.setChannelId(DEFAULT_CHANNEL_ID)
+        }
+
+        notificationManager.notify(NOTIFICATION_ID, notification.build())
+
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        if(BuildConfig.DEBUG)
+            Log.d("FireBaseMessaging", token)
+    }
+}
