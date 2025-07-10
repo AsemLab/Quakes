153f98437a7dba8c106203f5f64566301f851987
Asem Abu Alrub <asem.m.abualrub@gmail.com>
2025-07-10 10:50:45 +0300

    Upgrade to Android 15

--- buildSrc/src/main/java/com/asemlab/quakes/Configuration.kt
+++ buildSrc/src/main/java/com/asemlab/quakes/Configuration.kt
@@ -1,12 +1,12 @@
 package com.asemlab.quakes
 
 object Configuration {
-  const val compileSdk = 34
-  const val targetSdk = 34
+  const val compileSdk = 35
+  const val targetSdk = 35
   const val minSdk = 22
   const val majorVersion = 1
   const val minorVersion = 0
-  const val patchVersion = 3
+  const val patchVersion = 4
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 6
+  const val versionCode = 7
 }
